f0c48c5d87f22557a8035c520a98ddb6711c435d
wsnk <wsnk00@yandex.com>
2026-04-23 10:19:57 +0300

    More for git (#2)
    * More for git
    
    * Up

--- kotkotool/git.py
+++ kotkotool/git.py
@@ -23,6 +23,14 @@ class RemoteRepository:
     name: str
     urls: list[RemoteUrlT] = field(default_factory=list)
 
+    def __contains__(self, url: str) -> bool:
+        """Return if the remote has the given URL configured.
+        """
+        for it in self.urls:
+            if it[0] == url:
+                return True
+        return False
+
     def has_url(self, url: str) -> bool:
         # FIXME
         for it in self.urls:
@@ -42,12 +50,16 @@ class Remotes:
         for (name, url, mode) in parse_remotes(proc.stdout):
             rr = ret.setdefault(name, RemoteRepository(name=name))
             rr.urls.append((url, mode))
+            dbg("Remote found: name='%s', URL='%s', mode='%s'", name, url, mode)
 
         return ret
     
     async def set_remote(self, url, name="origin"):
         """
         Ensure the named remote is configured with the given URL.
+
+        If a remote with the given name already exists but has a different URL, it will be removed
+        and re-added with the new URL.
         """
 
         remotes = await self.get_remotes()
@@ -75,30 +87,80 @@ class GitRepository:
         await run_async([git_bin, "clone", url, str(dest_dir)])
         return cls(dest_dir)
 
-    def __init__(self, repo_dir: Path, *, git_bin=None):
-        self.repo_dir = repo_dir
+    @classmethod
+    async def init(cls, dest_dir: Path, *, git_bin=None):
+        """
+        Initialize a new git repository in the given directory. If the directory does not exist, it
+        will be created.
+        """
+
+        repo = cls(dest_dir, git_bin=git_bin)
+        await repo.run(["init", "."])
+        return repo
+
+    @classmethod
+    async def ensure_repo(cls, dest_dir: Path, *, initial_branch="master", origin=None):
+        dest_dir.mkdir(parents=True, exist_ok=True)
+
+        repo = cls(dest_dir)
+
+        if not repo.is_repository():
+            await repo.run(["init", "."])
+
+        if origin is not None:
+            await repo.remotes.set_remote(origin)
+
+        return repo
+
+    def __init__(self, repo_dir: str | Path, *, git_bin=None):
+        self.repo_dir = Path(repo_dir)
         self.git_bin = git_bin or "git"
 
     async def run(self, args, **kwargs):
+        args = tuple(str(it) for it in args if it is not None)
         return await run_async(
-            [self.git_bin, *(str(it) for it in args)],
+            [self.git_bin, *args],
             cwd=self.repo_dir,
             **kwargs
         )
+
+    def is_repository(self) -> bool:
+        return (self.repo_dir/".git").is_dir()
 
     @property
     def remotes(self) -> Remotes:
         return Remotes(self)
 
-    async def init(self):
-        await self.run(["init", "."])
-    
+    async def has_commit(self, refspec) -> bool:
+        """Return if the repository has/know the given refspec.
+        """
+        try:
+            res = await self.run(
+                ["cat-file", "-t", refspec],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.DEVNULL,
+                text=True
+            )
+            return res.stdout.strip() == "commit"
+        except subprocess.CalledProcessError:
+            return False
+
+    async def pull(self, remote=None, refspec=None):
+        await self.run(["pull", remote, refspec])
+
     async def checkout(self, branch):
         await self.run(["checkout", branch])
+    
+    async def fetch(self, repository=None, refspec=None, *, all=False):
+        """git-fetch - fetch refs from one or more remote repositories.
+        """
+        if all:
+            await self.run(["fetch", "--all"])
+        else:
+            await self.run(["fetch", repository, refspec])
 
     async def fetch_light(self, refspec, remote="origin"):
-        """
-        Fetch given <refspec> from a remote repository without blobs.
+        """Fetch given <refspec> from a remote repository without blobs.
         """
         await self.run([
             "fetch",
--- tests/test_git.py
+++ tests/test_git.py
@@ -14,8 +14,48 @@ async def test_get_commit_hash(git_repo_dir: str):
     """
 
     repo = GitRepository(git_repo_dir)
+
     commit_hash = await repo.get_commit_hash()
     assert len(commit_hash) == 40  # commit hash should be 40 characters long
+
+
+async def test_has_commit(git_repo_dir: str):
+    repo = GitRepository(git_repo_dir)
+    commit_hash = await repo.get_commit_hash()
+
+    assert (await repo.has_commit(commit_hash)) is True
+    assert (await repo.has_commit("master")) is True
+
+    assert (await repo.has_commit(f"{commit_hash}1")) is False
+    assert (await repo.has_commit("not-existing-refspec")) is False
+
+
+async def test_git_remotes(tmp_path):
+    """
+    Test getting and setting git remotes.
+    """
+
+    URL1 = "https://example.com/repo-1.git"
+    URL2 = "https://example.com/repo-2.git"
+
+    repo = await GitRepository.init(tmp_path)
+
+    # no remotes by default
+    assert (await repo.remotes.get_remotes()) == {}
+
+    # add remote and check
+    await repo.remotes.set_remote(URL1)
+
+    remotes = await repo.remotes.get_remotes()
+    assert len(remotes) == 1
+    assert URL1 in remotes["origin"]
+
+    # change remote URL and check
+    await repo.remotes.set_remote(URL2)
+
+    remotes = await repo.remotes.get_remotes()
+    assert len(remotes) == 1
+    assert URL2 in remotes["origin"]
 
 
 async def test_clone_git_repository(git_repo_dir: str, tmp_path):
@@ -29,3 +69,19 @@ async def test_clone_git_repository(git_repo_dir: str, tmp_path):
 
     assert repo.repo_dir.exists()
     await repo.get_commit_hash()  # no exception means the repository is valid
+
+
+async def test_ensure_git_repository(git_repo_dir: str, tmp_path):
+    url = f"file://{git_repo_dir}"
+    dest_dir = tmp_path / "a-repo"
+
+    remote_head_commit = await GitRepository(git_repo_dir).get_commit_hash()
+
+    repo = await GitRepository.ensure_repo(dest_dir, origin=url)
+    repo = await GitRepository.ensure_repo(dest_dir, origin=url)
+
+    assert repo.repo_dir.exists()
+    assert url in (await repo.remotes.get_remotes())["origin"]
+
+    await repo.pull("origin", "master")
+    assert (await repo.get_commit_hash()) == remote_head_commit
